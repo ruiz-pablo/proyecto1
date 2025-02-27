78993feef6711ee9a082481ce2a803f1c60e4f18
ruiz-pablo <pablo785@proton.me>
2025-02-27 19:15:09 +0100

    Remove unnecessary imports

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,15 +1,10 @@
 package controller;
 
-import database.Database;
 import database.DatabaseExamples;
-import database.DatabaseTesting;
-import model.Bill;
-import model.Client;
-import model.Product;
 
 public class Main {
 	public static void main(String[] args) {
-		// Ejercutar test sobre la base de datos
+		// Ejercutar tests sobre la base de datos
 		// DatabaseTesting.test();
 
 		// Ejecutar ejemplos de como usar la base de datos
