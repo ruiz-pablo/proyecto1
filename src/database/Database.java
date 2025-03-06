715c5bcebdc7c295686cfc4ccede17613a97a3de
ruiz-pablo <pablo785@proton.me>
2025-03-06 12:56:37 +0100

    Make tables final

--- src/database/Database.java
+++ src/database/Database.java
@@ -6,8 +6,8 @@ import model.Product;
 import model.SoldProduct;
 
 public class Database {
-	public static AbstractDatabaseTable<Client> clients = new AbstractDatabaseTable<Client>();
-	public static AbstractDatabaseTable<Product> products = new AbstractDatabaseTable<Product>();
-	public static AbstractDatabaseTable<Bill> bills = new AbstractDatabaseTable<Bill>();
-	public static AbstractDatabaseTable<SoldProduct> soldProducts = new AbstractDatabaseTable<SoldProduct>();
+	public static final AbstractDatabaseTable<Client> clients = new AbstractDatabaseTable<Client>();
+	public static final AbstractDatabaseTable<Product> products = new AbstractDatabaseTable<Product>();
+	public static final AbstractDatabaseTable<Bill> bills = new AbstractDatabaseTable<Bill>();
+	public static final AbstractDatabaseTable<SoldProduct> soldProducts = new AbstractDatabaseTable<SoldProduct>();
 }
