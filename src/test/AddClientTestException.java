b1bba0c925830c9da970a7789d952518816da350
ruiz-pablo <pablo785@proton.me>
2025-04-15 17:31:18 +0200

    Add templatest to test exceptions thrown by methods

---
+++ src/test/AddClientTestException.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddClientTestException {
+
+	@Before
+	public void setUp() throws Exception {
+		// Añadir aqui los clientes para comprobar que no se puede añadir un cliente repetido
+		// Database.clients.insertClient(new Client("nombre", "apellidos"...));
+	}
+
+	@Test
+	public void testAddClient() {
+		fail("Not yet implemented");
+	}
+}
