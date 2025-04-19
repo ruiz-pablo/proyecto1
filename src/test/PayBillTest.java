842956c6bd76ada6407f2f2fed46c66c7e6ce681
ruiz-pablo <pablo785@proton.me>
2025-04-19 13:09:32 +0200

    Fix: Change test using @Before to @BeforeClass

--- src/test/PayBillTest.java
+++ src/test/PayBillTest.java
@@ -2,13 +2,13 @@ package test;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PayBillTest {
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUp() throws Exception {
 	}
 
 	@Test
--- src/test/PayBillTestException.java
+++ src/test/PayBillTestException.java
@@ -2,13 +2,13 @@ package test;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PayBillTestException {
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUp() throws Exception {
 		// Añadir aqui los clientes para comprobar que no se puede añadir un cliente repetido
 		// Database.clients.insertClient(new Client("nombre", "apellidos"...));
 	}
